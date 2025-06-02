cb18b050881e464a05a9cc588cf753b8a5d03bad
grayzy2617 <phat058zz@gmail.com>
2025-06-02 16:12:30 +0700

    change branch

--- app/src/main/java/pbl3_gradle/views/ProfileMemberPage.java
+++ app/src/main/java/pbl3_gradle/views/ProfileMemberPage.java
@@ -11,24 +11,24 @@ public class ProfileMemberPage {
         public Pane getView() {
                 // Tao menu bar
                 Pane menuBar = MenuBarStyle_Layer2(
-                                "file:src/main/resources/image/ImageAvatar.png", "Nguyễn Thị Huyền Trang",
-                                "Scrum Master",
-                                "ProfileMemberPage");
+                        "file:src/main/resources/image/ImageAvatar.png", "Nguyễn Thị Huyền Trang",
+                        "Scrum Master",
+                        "ProfileMemberPage");
                 // Tao Avatar iamge
                 Image image1 = new Image(
-                                "file:src/main/resources/image/ImageAvatar.png");
+                        "file:src/main/resources/image/ImageAvatar.png");
                 AvatarViewClass avatar1 = new AvatarViewClass(image1, 205.7, 4);
                 avatar1.setLayoutX(732.6);
                 avatar1.setLayoutY(18.2);
                 // Tao button doi avatar
                 Image image2 = new Image(
-                                "file:src/main/resources/image/ChangeAvatar.png");
+                        "file:src/main/resources/image/ChangeAvatar.png");
                 AvatarViewClass avatar2 = new AvatarViewClass(image2, 46.1, 0);
                 ImageButtonClass btnChange = new ImageButtonClass(avatar2, 46.1, 46.1, 879.3, 178.3);
                 // Tao label ten
                 Label nameLb = new Label("Nguyễn Thị Huyền Trang");
                 nameLb.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 20 px; -fx-alignment: center; -fx-font-family: :'Helvetica';");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 20 px; -fx-alignment: center; -fx-font-family: :'Helvetica';");
                 nameLb.setPrefSize(443.1, 31.7);
                 nameLb.setLayoutX(613.9);
                 nameLb.setLayoutY(245);
@@ -42,7 +42,7 @@ public class ProfileMemberPage {
                 Pane pane = new Pane();
                 pane.getChildren().addAll(menuBar, avatar1, btnChange, nameLb, rect, gridPane);
                 pane.setStyle(
-                                "-fx-background-color: #ffffff; -fx-background-sdize: cover; ");
+                        "-fx-background-color: #ffffff; -fx-background-sdize: cover; ");
                 pane.setPrefSize(1366, 768);
                 pane.setLayoutX(0);
                 pane.setLayoutY(0);
@@ -61,14 +61,14 @@ public class ProfileMemberPage {
                 // Tao Label cho avatar
                 Label avarLb = new Label(text);
                 avarLb.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 15 px; -fx-alignment: center; -fx-font-family: :'Helvetica';");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 15 px; -fx-alignment: center; -fx-font-family: :'Helvetica';");
                 avarLb.setPrefSize(276.8, 24);
                 avarLb.setLayoutX(9.7);
                 avarLb.setLayoutY(167.6);
                 // Tao Label cho role
                 Label roleLb = new Label(role);
                 roleLb.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 12 px; -fx-alignment: center; -fx-font-family: :'Helvetica';");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 12 px; -fx-alignment: center; -fx-font-family: :'Helvetica';");
                 roleLb.setPrefSize(148.7, 18.8);
                 roleLb.setLayoutX(73.7);
                 roleLb.setLayoutY(197.6);
@@ -79,7 +79,7 @@ public class ProfileMemberPage {
                 Pane pane = new Pane();
                 pane.getChildren().addAll(rect, avatar, avarLb, roleLb, menuBar);
                 pane.setStyle(
-                                "-fx-background-color: #ffffff; -fx-background-size: cover;");
+                        "-fx-background-color: #ffffff; -fx-background-size: cover;");
                 pane.setPrefSize(296.1, 768);
                 pane.setLayoutX(0);
                 pane.setLayoutY(0);
@@ -99,24 +99,24 @@ public class ProfileMemberPage {
                 Label lb9 = new Label(roleText);
 
                 lb1.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb2.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb3.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb4.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
 
                 lb5.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb6.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb7.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb8.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb9.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;");
                 lb1.setPrefSize(273.2, 57.2);
                 lb2.setPrefSize(273.2, 57.2);
                 lb3.setPrefSize(273.2, 57.2);
@@ -141,29 +141,29 @@ public class ProfileMemberPage {
                 TextField tf1 = new TextField();
                 tf1.setPromptText("Enter your full name here");
                 tf1.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px; -fx-background-color: transparent;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px; -fx-background-color: transparent;");
                 tf1.setPrefSize(593.9, 57.2);
 
                 TextField tf2 = new TextField();
                 tf2.setPromptText("Enter your English name here");
                 tf2.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
                 tf2.setPrefSize(593.9, 57.2);
 
                 TextField tf3 = new TextField();
                 tf3.setPromptText("Enter your date of birth here");
                 tf3.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
                 tf3.setPrefSize(593.9, 57.2);
                 TextField tf4 = new TextField();
                 tf4.setPromptText("Enter your address here");
                 tf4.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
                 tf4.setPrefSize(593.9, 57.2);
                 TextField tf5 = new TextField();
                 tf5.setPromptText("Enter your phone number here");
                 tf5.setStyle(
-                                "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
+                        "-fx-text-fill: #2f74eb; -fx-font-size: 14 px; -fx-alignment: center; -fx-font-family: :'Helvetica'; -fx-border-color: #ffffff; -fx-border-width:1px;-fx-background-color: transparent;");
                 tf5.setPrefSize(593.9, 57.2);
                 gridPane.add(tf1, 1, 1);
                 gridPane.add(tf2, 1, 2);
